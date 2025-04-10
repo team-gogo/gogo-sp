f0f302c78da9d0b3887e8b1c8b9803689dba52bc
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-10 20:08:01 +0900

    fix: inverse serialization problem

--- src/main/kotlin/gogo/gogosp/global/internal/user/stub/Sex.kt
+++ src/main/kotlin/gogo/gogosp/global/internal/user/stub/Sex.kt
@@ -1,5 +1,10 @@
 package gogo.gogosp.global.internal.user.stub
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class Sex {
-    MALE, FEMALE
+    @JsonProperty("MALE")
+    MALE,
+    @JsonProperty("FEMALE")
+    FEMALE
 }
